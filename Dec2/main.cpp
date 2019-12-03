c1492cbe44d9ca0c45b6c16cc1012eff74fd3db9
Rick Twohy <twohyjr@gmail.com>
2019-12-02 23:34:52 -0500

    Dec 2 Completed

--- Dec2/main.cpp
+++ Dec2/main.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <sstream>
 
-
 std::vector<int> readComputerProgramOpcodes(std::string filepath) {
     std::vector<int> massValues;
     std::ifstream inputFile;
@@ -62,11 +61,10 @@ int intCodeProgramFirstValue(std::vector<int> opcodes, int noun, int verb) {
                 break;
             case 99:
                 programIsTerminated = true;
-                // std::cout << "Program Terminated" << std::endl;
                 break;
             default:
                 programIsTerminated = true;
-                // std::cout << "Something horrible has happened" << std::endl;
+                std::cout << "Something horrible has happened" << std::endl;
                 break;
         }
         
@@ -79,15 +77,13 @@ int intCodeProgramFirstValue(std::vector<int> opcodes, int noun, int verb) {
     return opcodes[0];
 }
 
-int main() {
-    auto opcodes = readComputerProgramOpcodes("Dec2/input");
-    
+int bruteForceTheShitOutOfTheAnswer(std::vector<int> &opcodes, int valueToFind, int iterations) {
     bool foundNounAndVerb = false;
     int noun = 0, verb = 0;
-    for(int i = 0; i < 10000; i++) {
-        for(int j = 0; j < 10000; j++) {
+    for(int i = 0; i < iterations; i++) {
+        for(int j = 0; j < iterations; j++) {
             int intCodeProgramValue = intCodeProgramFirstValue(opcodes, i, j);
-            if(intCodeProgramValue == 19690720) {
+            if(intCodeProgramValue == valueToFind) {
                 foundNounAndVerb = true;
                 noun = i;
                 verb = j;
@@ -98,19 +94,36 @@ int main() {
             break;
         }
     }
-    std::cout << "NOUN: " << noun << " -- VERB: " << verb << std::endl;
-    std::cout << "The final answer is: " << 100 * noun + verb << std::endl;
+    if(foundNounAndVerb) {
+        std::cout << "The noun is: " << noun << "\n" << "The verb is: " << verb << std::endl;
+    } else {
+        // Increase iterations
+        return -1;
+    }
     
-    
+    return 100 * noun + verb;
 }
 
-// Opcodes - read positioins
-//       1    -   Adds together numbers read from two positions and stores
-//                the result in a third position
-//
-//       2    -   works exactly like opcode 1, except it multiplies the
-//                two inputs instead of adding them
-//
-//      99    -  program is finished and should immediately halt.
-//
-// Unknown    -  something went wrong
+void printSolution(std::vector<int> &opcodes, int iterationIncrement) {
+    int finalValue = -1;
+    int iterationCount = 0;
+    while(finalValue == -1) {
+        finalValue = bruteForceTheShitOutOfTheAnswer(opcodes, 19690720, iterationIncrement * iterationCount + iterationIncrement);
+        iterationCount += 1;
+    }
+    std::cout << "The final answer is: " << finalValue << std::endl;
+    std::cout << "It took " << iterationCount << " iteration(s) using " << iterationCount << "!" << std::endl;
+    if(iterationCount > 1) {
+        std::cout << "You should the increase iteration increment to " << iterationIncrement * iterationCount + iterationIncrement << " for faster results" << std::endl;
+    }
+}
+
+int main() {
+    // increase / decrease this value if you cannot find the value fast enough
+    int iterationIncrement = 91;
+
+    auto opcodes = readComputerProgramOpcodes("Dec2/input");
+    printSolution(opcodes, iterationIncrement);
+    
+    return 0;
+}
