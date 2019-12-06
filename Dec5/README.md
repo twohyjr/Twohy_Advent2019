35dcf05a8966b701e4c53989974a77ab6121d415
Rick Twohy <twohyjr@gmail.com>
2019-12-05 20:57:12 -0500

    Finished Dec5

--- Dec5/README.md
+++ Dec5/README.md
@@ -10,6 +10,15 @@ Intcode computer after a few modifications:
 
 First, you'll need to add two new instructions:
 
+Opcode 1 adds together numbers read from two positions and stores the result in a 
+third position. The three integers immediately after the opcode tell you these three 
+positions - the first two indicate the positions from which you should read the input 
+values, and the third indicates the position at which the output should be stored.
+
+Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of 
+adding them. Again, the three integers after the opcode indicate where the inputs and 
+outputs are, not their values.
+
 Opcode 3 takes a single integer as input and saves it to the position given by its 
 only parameter. For example, the instruction 3,50 would take an input value and 
 store it at address 50.
--- Dec5/main.cpp
+++ Dec5/main.cpp
@@ -75,7 +75,10 @@ OpcodeProgram getOpcodeProgram(int programInstruction) {
     };
 }
 
-
+//Adds together numbers read from two positions and stores the result in a
+//third position. The three integers immediately after the opcode tell you these three
+//positions - the first two indicate the positions from which you should read the input
+//values, and the third indicates the position at which the output should be stored.
 void processOpcode1(std::vector<int> &opcodes,
                     OpcodeProgram program,
                     int param1,
@@ -83,9 +86,13 @@ void processOpcode1(std::vector<int> &opcodes,
                     int param3) {
     int value1 = program.param1InputMode == InputModes::POSITION ? opcodes[param1] : param1;
     int value2 = program.param2InputMode == InputModes::POSITION ? opcodes[param2] : param2;
+    
     opcodes[param3] = value1 + value2;
 }
 
+//Works exactly like opcode 1, except it multiplies the two inputs instead of
+//adding them. Again, the three integers after the opcode indicate where the inputs and
+//outputs are, not their values.
 void processOpcode2(std::vector<int> &opcodes,
                     OpcodeProgram program,
                     int param1,
@@ -93,23 +100,98 @@ void processOpcode2(std::vector<int> &opcodes,
                     int param3) {
     int value1 = program.param1InputMode == InputModes::POSITION ? opcodes[param1] : param1;
     int value2 = program.param2InputMode == InputModes::POSITION ? opcodes[param2] : param2;
+    
     opcodes[param3] = value1 * value2;
 }
 
-// Write value to index
+//Takes a single integer as input and saves it to the position given by its
+//only parameter. For example, the instruction 3,50 would take an input value and
+//store it at address 50.
 void processOpcode3(std::vector<int> &opcodes,
-                    int value,
-                    int writeIndex) {
-    opcodes[writeIndex] = value;
+                    OpcodeProgram program,
+                    int param1,
+                    int id) {
+    opcodes[param1] = id;
 }
 
-// Returns output at address
+//Outputs the value of its only parameter. For example, the instruction 4,50
+//would output the value at address 50.
 int processOpcode4(std::vector<int> const &opcodes,
                    int address) {
     return opcodes[address];
 }
 
-// Executes the opcodes program
+//jump-if-true: if the first parameter is non-zero, it sets the instruction
+//pointer to the value from the second parameter. Otherwise, it does nothing.
+void processOpcode5(std::vector<int> &opcodes,
+                    OpcodeProgram program,
+                    int param1,
+                    int param2,
+                    int &instructionPointer) {
+    int value1 = program.param1InputMode == InputModes::POSITION ? opcodes[param1] : param1;
+    int value2 = program.param2InputMode == InputModes::POSITION ? opcodes[param2] : param2;
+    
+    if(value1 != 0) {
+        instructionPointer = value2;
+    } else {
+        instructionPointer += 3;
+    }
+}
+
+//jump-if-false: if the first parameter is zero, it sets the instruction pointer
+//to the value from the second parameter. Otherwise, it does nothing.
+void processOpcode6(std::vector<int> &opcodes,
+                    OpcodeProgram program,
+                    int param1,
+                    int param2,
+                    int &instructionPointer) {
+    int value1 = program.param1InputMode == InputModes::POSITION ? opcodes[param1] : param1;
+    int value2 = program.param2InputMode == InputModes::POSITION ? opcodes[param2] : param2;
+    
+    if(value1 == 0) {
+        instructionPointer = value2;
+    } else {
+        instructionPointer += 3;
+    }
+}
+
+//less than: if the first parameter is less than the second parameter, it
+//stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+void procesOpcode7(std::vector<int> &opcodes,
+                   OpcodeProgram program,
+                   int param1,
+                   int param2,
+                   int param3) {
+    int value1 = program.param1InputMode == InputModes::POSITION ? opcodes[param1] : param1;
+    int value2 = program.param2InputMode == InputModes::POSITION ? opcodes[param2] : param2;
+    int value3 = param3;
+
+    if(value1 < value2) {
+        opcodes[value3] = 1;
+    } else {
+        opcodes[value3] = 0;
+    }
+}
+
+//equals: if the first parameter is equal to the second parameter, it
+//stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+void procesOpcode8(std::vector<int> &opcodes,
+                   OpcodeProgram program,
+                   int param1,
+                   int param2,
+                   int param3) {
+    int value1 = program.param1InputMode == InputModes::POSITION ? opcodes[param1] : param1;
+    int value2 = program.param2InputMode == InputModes::POSITION ? opcodes[param2] : param2;
+    int value3 = param3;
+
+    if(value1 == value2) {
+        opcodes[value3] = 1;
+    } else {
+        opcodes[value3] = 0;
+    }
+}
+
+// Executes the opcodes program based on the given id
 int runProgram(std::vector<int> &opcodes, int id) {
     std::vector<int> outputs;
 
@@ -119,9 +201,10 @@ int runProgram(std::vector<int> &opcodes, int id) {
         if(instructionPointer == opcodes.size() - 1) {
             programIsTerminated = true;
         }
-        
+
         OpcodeProgram program = getOpcodeProgram(opcodes[instructionPointer]);
         int opcode = program.opcode;
+        
         switch (opcode) {
             case 1:
                 processOpcode1(opcodes,
@@ -140,12 +223,45 @@ int runProgram(std::vector<int> &opcodes, int id) {
                 instructionPointer += 4;
                 break;
             case 3:
-                opcodes[opcodes[instructionPointer + 1]] = id;
+                processOpcode3(opcodes,
+                               program,
+                               opcodes[instructionPointer + 1],
+                               id);
                 instructionPointer += 2;
                 break;
             case 4:
                 outputs.push_back(processOpcode4(opcodes, opcodes[instructionPointer + 1]));
                 instructionPointer += 2;
+                break;
+            case 5:
+                processOpcode5(opcodes,
+                               program,
+                               opcodes[instructionPointer + 1],
+                               opcodes[instructionPointer + 2],
+                               instructionPointer);
+                break;
+            case 6:
+                processOpcode6(opcodes,
+                               program,
+                               opcodes[instructionPointer + 1],
+                               opcodes[instructionPointer + 2],
+                               instructionPointer);
+                break;
+            case 7:
+                procesOpcode7(opcodes,
+                              program,
+                              opcodes[instructionPointer + 1],
+                              opcodes[instructionPointer + 2],
+                              opcodes[instructionPointer + 3]);
+                instructionPointer += 4;
+                break;
+            case 8:
+                procesOpcode8(opcodes,
+                              program,
+                              opcodes[instructionPointer + 1],
+                              opcodes[instructionPointer + 2],
+                              opcodes[instructionPointer + 3]);
+                instructionPointer += 4;
                 break;
             case 99:
                 programIsTerminated = true;
@@ -156,25 +272,23 @@ int runProgram(std::vector<int> &opcodes, int id) {
                 break;
         }
     }
-    
-    for(int i = 1; i < outputs.size() - 1; i++) {
-        if(outputs[i] != 0) {
-            std::cout << "Output with the value of: " << outputs[i] << " is incorrect" << std::endl;
-            return -1;
-        }
-        
-    }
-    
-    return outputs[outputs.size() - 1];
+
+    return id == 1 ? outputs[outputs.size() - 1] : outputs[0];
 }
 
 int main(int argc, const char * argv[]) {
-    auto opcodes = readComputerProgramOpcodes("Dec5_input");
     
     // Part 1
-    int programOutput1 = runProgram(opcodes, 1);
-    std::cout << "The final program output is: " << programOutput1 << std::endl;
+    auto opcodes1 = readComputerProgramOpcodes("Dec5_input");
+    int programOutput1 = runProgram(opcodes1, 1);
+    std::cout << "Part 1: The final program output is: " << programOutput1 << std::endl;
     // Answer -- 15386262
-
+    
+    // Part 2
+    auto opcodes2 = readComputerProgramOpcodes("Dec5_input");
+    int programOutput2 = runProgram(opcodes2, 5);
+    std::cout << "Part 2: The final program output is: " << programOutput2 << std::endl;
+    // Answer -- 10376124
+    
     return 0;
 }
